6fad0298ff1b8c69fd9357daadd1e721a9e912fe
NIGAM YADAV <71872897+nigamyadav72@users.noreply.github.com>
2026-02-10 22:48:22 +0545

    hello world

--- todo_list_app/lib/home_page.dart
+++ todo_list_app/lib/home_page.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
-class HomePage extends StatelessWidget {
 
+class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Row(children: [Center(
+        
+        child: Text('hello world'))]),
     );
   }
 }
