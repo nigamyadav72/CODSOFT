ee5a22d5abd969388a676f0c56ebf28e5baf2171
NIGAM YADAV <71872897+nigamyadav72@users.noreply.github.com>
2026-02-13 23:27:00 +0545

    ongoing

--- todo_list_app/lib/home_page.dart
+++ todo_list_app/lib/home_page.dart
@@ -6,11 +6,34 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Row(
-        children: [
-          Center(child: Text('hello world')),
-          Image.asset('image.jpg'),
-        ],
+      appBar: AppBar(
+        leading: Icon(Icons.table_rows),
+        actions: [Icon(Icons.search), Icon(Icons.notifications)],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Hello Mohin',
+              style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 5),
+            Text('25 Task are pending'),
+            SizedBox(height: 20),
+            TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(40)),
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Text('Ongoing Task'),
+            Card(color: Colors.black),
+          ],
+        ),
       ),
     );
   }
---
+++ todo_list_app/lib/welcome_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class WelcomePage extends StatelessWidget {
+  const WelcomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Row(children: [Text('Hello this is welcome page.')]));
+  }
+}
